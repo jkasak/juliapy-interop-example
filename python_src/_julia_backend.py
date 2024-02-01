efa322bc77c0ece92506e491a60d9a6356ea843e
Jaan Kasak <jaan.kasak@abzu.ai>
2024-02-01 13:41:17 +0100

    remember to instantiate the package

--- python_src/_julia_backend.py
+++ python_src/_julia_backend.py
@@ -6,6 +6,7 @@ repo_root_path = Path(__file__).parent.parent
 julia_module_path = str(repo_root_path / "julia_src" / "MyJuliaModule.jl")
 
 jl.Pkg.activate(str(repo_root_path), io=jl.devnull)
+jl.Pkg.instantiate(io=jl.devnull)
 jl.seval(f'include("{julia_module_path}")')
 
 # Functions in MyJuliaModule are now in this namespace
