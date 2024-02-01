6c8a8b63d61b0b2e0ba572d5b6b4dad1fff0d830
Jaan Kasak <jaan.kasak@abzu.ai>
2024-02-01 11:17:34 +0100

    tune python part

--- demo_script.py
+++ demo_script.py
@@ -1,14 +1,6 @@
 import python_src
+from pathlib import Path
 
-json_data = """
-[
-  {"name": "Laptop", "category": "Electronics", "price": 1200},
-  {"name": "Desk", "category": "Furniture", "price": 450},
-  {"name": "Chair", "category": "Furniture", "price": 150},
-  {"name": "Smartphone", "category": "Electronics", "price": 800},
-  {"name": "Table Lamp", "category": "Furniture", "price": 90}
-]
-"""
+price = python_src.average_furniture_price(Path("./demo_data.json"))
 
-price = python_src.average_furniture_price(json_data)
 print(f"The average price of furniture is {price}€.")
