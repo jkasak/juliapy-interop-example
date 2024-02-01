19174faa933b4f1a6a305ad63c0feb8d7cee0424
Jaan Kasak <jaan.kasak@abzu.ai>
2024-02-01 11:22:38 +0100

    Add equivalent julia function

--- julia_src/MyJuliaModule.jl
+++ julia_src/MyJuliaModule.jl
@@ -2,4 +2,14 @@ module MyJuliaModule
 
 using JSON
 
+
+function average_furniture_price(json_path::String)::Float64
+    data = JSON.parsefile(json_path)
+
+    furniture_products = filter(product -> product["category"] == "Furniture", data)
+    total_price = sum(product["price"] for product in furniture_products)
+
+    return total_price / length(furniture_products)
+end
+
 end
